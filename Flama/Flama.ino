dc80d5b0133c6fe383b37111351a09858aba0850
Makako08 <marcoalonso24@hotmail.com>
2023-06-01 13:27:57 -0600

    Update Flama.ino

--- Flama/Flama.ino
+++ Flama/Flama.ino
@@ -1,31 +1,25 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
 
- modified 8 Nov 2013
- by Scott Fitzgerald
- https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
-*/
+
+/******************************************************************/
+/***********Código para probar el movimiento de la garra***********/
 
 #include <Servo.h>
+Servo myservo; 
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
-int pos = 0;    // variable to store the servo position
 
 void setup() {
-  myservo.attach(3);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(3);  
 }
 
 void loop() {
-for (pos = 35; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+for (pos = 35; pos <= 90; pos += 1) { 
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(10);                       // waits 15 ms for the servo to reach the position
+    myservo.write(pos);              
+    delay(10);                       
   }
-  for (pos = 90; pos >= 35; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+  for (pos = 90; pos >= 35; pos -= 1) { 
+    myservo.write(pos);              
     delay(10); 
   }
 }
+/*****************************************************************/
