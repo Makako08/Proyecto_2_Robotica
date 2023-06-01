89be61489464784687593138c23dc6e40e7e12f5
Makako08 <marcoalonso24@hotmail.com>
2023-06-01 14:31:59 -0600

    update

--- Flama/Flama.ino
+++ Flama/Flama.ino
@@ -1,8 +1,39 @@
 
+/******************************************************************/
+/********************Código para mover la garra********************/
+
+#include <Servo.h>
+Servo servo1;
+Servo servo2;
+Servo servo3; 
+
+float x;
+void setup() {
+  servo1.attach(3);
+  servo2.attach(5);
+  servo3.attach(6);
+}
+
+void loop() {
+  x = map(analogRead(A0), 0, 1023, 35, 90);
+  
+  servo1.write(x);
+  servo2.write(x);
+  servo3.write(x);
+
+}
+
+/******************************************************************/
+
+
+
+
+
+
 
 /******************************************************************/
 /***********Código para probar el movimiento de la garra***********/
-
+/*
 #include <Servo.h>
 Servo myservo; 
 
@@ -22,4 +53,5 @@ for (pos = 35; pos <= 90; pos += 1) {
     delay(10); 
   }
 }
+*/
 /*****************************************************************/
