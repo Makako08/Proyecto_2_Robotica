37a783791da00be4d423cc2ab247186968694278
Makako08 <marcoalonso24@hotmail.com>
2023-06-16 09:35:43 -0600

    Update Flama.ino

--- Programaciones/Flama/Flama.ino
+++ Programaciones/Flama/Flama.ino
@@ -1,11 +1,10 @@
-
 /******************************************************************/
 /********************Código para mover la garra********************/
 
 #include <Servo.h>
 Servo servo1;
-Servo servo2;
-Servo servo3; 
+//Servo servo2;
+//Servo servo3; 
 
 int x;
 int pot;
@@ -13,17 +12,17 @@ int pot;
 void setup() {
   pinMode(A0, INPUT);
   servo1.attach(3);
-  servo2.attach(5);
-  servo3.attach(6);
+  //servo2.attach(5);
+  //servo3.attach(6);
 }
 
 void loop() {
   pot = analogRead(A0);
-  x = map(pot, 0, 1023, 55, 115);
+  x = map(pot, 0, 1023, 35, 110);
   
   servo1.write(x);
-  servo2.write(x);
-  servo3.write(x);
+  //servo2.write(x);
+  //servo3.write(x);
 
 }
 
