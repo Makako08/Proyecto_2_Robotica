663c18008cab7bc99184756d47004a960100bd0f
Makako08 <marcoalonso24@hotmail.com>
2023-06-18 18:52:15 -0600

    Update Control_garra_robotica.ino

--- Programaciones/Control_garra_robotica/Control_garra_robotica.ino
+++ Programaciones/Control_garra_robotica/Control_garra_robotica.ino
@@ -19,8 +19,8 @@ int deg = 90;
 int r1 = 13;
 int r2 = 12;
 
-int in1 = 11;
-int in2 = 3;
+int in1 = 2;
+int in2 = 4;
 int enA = 8;
 int vel = 255;
 
@@ -29,62 +29,105 @@ int pot;
 
 void setup() {
   pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
+  pinMode(A3, INPUT);
   pinMode(A2, INPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
   pinMode(r1, INPUT);
   pinMode(r2, INPUT);
   garra.attach(5);  
   Z.attach(9);
-  R.attach(6);
-  R.write(deg);
+  R.attach(3);
+  R.write(90);
+  Serial.begin(9600);
 }
 
 void loop() {
   //Bloquea el codo y deshabilita la muñeca
   Z.write(90);
-  digitalWrite(in1, 0);
-  digitalWrite(in2, 0);
+  //R.write(deg);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  Serial.print(deg);
+  Serial.print("       ");
+  Serial.print(analogRead(A0));
+  Serial.print("       ");
+  Serial.print(analogRead(A2));
+  Serial.print("       ");
+  Serial.println(pot);
+  
 
   //Gira a la izquierda el servo del hombro
   while(digitalRead(r1) == true && deg != 135){
     deg ++;
     R.write(deg);
-    delay(60);
+    Serial.println(deg);
+    delay(40);
   }
 
   //Gira a la derecha el servo del hombro
   while(digitalRead(r2) == true && deg != 45){
     deg --;
     R.write(deg);
-    delay(60);
+    Serial.println(deg);
+    delay(40);
   }
   
 //Mueve a la izquierda el codo
-  while(analogRead(A1) > 800){
-    analogWrite(in1, vel);
-    digitalWrite(in2, 0);
-    digitalWrite(enA, HIGH);
+  while(analogRead(A2) > 800){
+    digitalWrite(4, LOW);
+    digitalWrite(2, HIGH);
+    Serial.print(deg);
+    Serial.print("       ");
+    Serial.print(analogRead(A0));
+    Serial.print("       ");
+    Serial.print(analogRead(A2));
+    Serial.print("       ");
+    Serial.println(pot);
   }
 
   //Mueve a la derecha el codo
-  while(analogRead(A1) < 200){
-    digitalWrite(in1, 0);
-    analogWrite(in2, vel);
-    digitalWrite(enA, HIGH);
+  while(analogRead(A2) < 200){
+    digitalWrite(2, LOW);
+    digitalWrite(4, HIGH);
+    //Serial.print(deg);
+    Serial.print("       ");
+    Serial.print(analogRead(A0));
+    Serial.print("       ");
+    Serial.print(analogRead(A2));
+    Serial.print("       ");
+    Serial.println(pot);
   }
 
   //Sube la muñeca
   while(analogRead(A0) > 800){
     Z.write(0);
+    R.write(deg);
+   Serial.print(deg);
+    Serial.print("       ");
+    Serial.print(analogRead(A0));
+    Serial.print("       ");
+    Serial.print(analogRead(A2));
+    Serial.print("       ");
+    Serial.println(pot);
   }
 
   //Baja la muñeca
   while(analogRead(A0) < 200){
     Z.write(180);
+    //R.write(deg);
+   Serial.print(deg);
+    Serial.print("       ");
+    Serial.print(analogRead(A0));
+    Serial.print("       ");
+    Serial.print(analogRead(A2));
+    Serial.print("       ");
+    Serial.println(pot);
   }
 
   //Control para abrir y cerrar la garra
   pot = analogRead(A3);
+  
   //mov = map(pot, 0, 1023, 35, 110);
   if (pot < 500){
     garra.write(35);
